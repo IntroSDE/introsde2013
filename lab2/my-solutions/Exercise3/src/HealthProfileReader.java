411c9d69d42fe885f0e8e8bc9cc044244d672f9d
matax87 <matax87@gmail.com>
2013-10-08 16:17:16 +0200

    Updated exercise 3. Now set also the value and can add person.

--- lab2/my-solutions/Exercise3/src/HealthProfileReader.java
+++ lab2/my-solutions/Exercise3/src/HealthProfileReader.java
@@ -9,55 +9,32 @@ public class HealthProfileReader {
 	
 	public static Map<String,Person> database = new HashMap<String,Person>();
 	
-	public HealthProfileReader()
+	static
 	{
-	    initializeDatabase();
+	    Person pallino = new Person();
+		Person pallo = new Person("Pinco", "Pallo");
+		HealthProfile hp = new HealthProfile(68.0,1.72);
+		Person john = new Person("John", "Doe",hp);
+		
+		database.put(pallino.getFirstname() + " " + pallino.getLastname(), pallino);
+		database.put(pallo.getFirstname() + " " + pallo.getLastname(), pallo);
+		database.put(john.getFirstname() + " " + john.getLastname(), john);
 	}
 	
-	/**
-	 * The health profile reader gets information from the command line about
-	 * weight and height and calculates the BMI of the person based on this 
-	 * parameters
-	 * 
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		int argCount = args.length;
-		if (argCount == 0) {
-			System.out.println("I cannot create people out of thing air. Give me at least a name and lastname.");
-		} else if (argCount < 3) {
-			System.out.println("Are you sure you gave me a first, a lastname and a measure?");
-		} else if (argCount == 2) {
-			String fname = args[0];
-			String lname = args[1];
-			// read the person from the DB
-			Person p= database.get(fname + " " + lname);
-			if (p!=null) { 
-				System.out.println(fname + " " + lname + "'s health profile is: " + p.gethProfile().toString());
-			} else {
-				System.out.println(fname + " " + lname + " is not in the database");
-			}
-		} else if (argCount == 3) {
-			String fname = args[0];
-			String lname = args[1];
-			String measure = args[2];
-			// read the person from the DB
-			Person p = database.get(fname + " " + lname);
-			if (p != null) { 
-			    if (measure.equals("weight")) {
-				    System.out.println(fname + " " + lname + "'s weight is: " + p.gethProfile().getWeight());
-				} else if (measure.equals("height")) {
-				    System.out.println(fname + " " + lname + "'s height is: " + p.gethProfile().getHeight());
-				} else if (measure.equals("bmi")) {
-				    System.out.println(fname + " " + lname + "'s bmi is: " + p.gethProfile().getBMI());
-				} else {
-				    System.out.println("The only available options are \"weight\", \"height\" or \"bmi\"");
-				}			
-			} else {
-				System.out.println(fname + " " + lname + " is not in the database");
-			}
-		}
-		// add the case where there are 3 parameters, the third being a string that matches "weight", "height" or "bmi"
+	public void addPerson(String fname, String lname)
+	{
+	    Person p = new Person(fname, lname);
+	    database.put(p.getFirstname() + " " + p.getLastname(), p);
+	}
+	
+	public HealthProfile getHealthProfile(String fname, String lname)
+	{
+	    HealthProfile hp = null;
+	    Person p = database.get(fname + " " + lname);
+	    if (p != null) {
+	        hp = p.gethProfile();
+	    }
+	    return hp;
 	}
 	
 	public double getWeight(String fname, String lname)
@@ -70,6 +47,14 @@ public class HealthProfileReader {
 	    return w;
 	}
 	
+	public void setWeight(String fname, String lname, double value)
+	{
+	    Person p = database.get(fname + " " + lname);
+	    if (p != null) {
+	        p.gethProfile().setWeight(value);
+	    }
+	}
+	
 	public double getHeight(String fname, String lname)
 	{
 	    double h = 0;
@@ -80,6 +65,14 @@ public class HealthProfileReader {
 	    return h;
 	}
 	
+	public void setHeight(String fname, String lname, double value)
+	{
+	    Person p = database.get(fname + " " + lname);
+	    if (p != null) {
+	        p.gethProfile().setHeight(value);
+	    }
+	}
+	
 	public double getBMI(String fname, String lname)
 	{
 	    double bmi = 0;
@@ -88,17 +81,5 @@ public class HealthProfileReader {
 	        bmi = p.gethProfile().getBMI();
 	    }
 	    return bmi;
-	}
-	
-	public static void initializeDatabase()
-	{
-		Person pallino = new Person();
-		Person pallo = new Person("Pinco","Pallo");
-		HealthProfile hp = new HealthProfile(68.0,1.72);
-		Person john = new Person("John","Doe",hp);
-		
-		database.put(pallino.getFirstname()+" "+pallino.getLastname(), pallino);
-		database.put(pallo.getFirstname()+" "+pallo.getLastname(), pallo);
-		database.put(john.getFirstname()+" "+john.getLastname(), john);
 	}
 }
