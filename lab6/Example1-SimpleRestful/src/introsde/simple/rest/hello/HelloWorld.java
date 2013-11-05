dd21487cc01b3f7bc836d365043010f2272ba297
Cristhian Parra <cdparra@gmail.com>
2013-11-05 16:00:43 +0100

    Added Person Health Profile REST simple service

--- lab6/Example1-SimpleRestful/src/introsde/simple/rest/hello/HelloWorld.java
+++ lab6/Example1-SimpleRestful/src/introsde/simple/rest/hello/HelloWorld.java
@@ -7,6 +7,14 @@ import javax.ws.rs.core.MediaType;
 
 @Path("/helloworld")
 public class HelloWorld {
+
+	// This method is called if TEXT_PLAIN is request
+	@GET
+	@Produces(MediaType.TEXT_PLAIN)
+	public String sayPlainTextHello() {
+		return "Hello World in REST";
+	}
+
 	// When client wants XML
 	@GET
 	@Produces(MediaType.TEXT_XML)
@@ -22,5 +30,12 @@ public class HelloWorld {
 		return "<html> " + "<title>" + "Hello Jersey" + "</title>"
 				+ "<body><h1>" + "Hello World in REST" + "</body></h1>"
 				+ "</html> ";
+	}
+
+	// When client wants XML
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public String sayHelloJson() {
+		return "{ 'msg': 'Hello World in REST' }";
 	}
 }
