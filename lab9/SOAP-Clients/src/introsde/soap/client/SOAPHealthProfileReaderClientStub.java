da5cf2e291ac5b6950f404c63ad62197fe97e5f3
Cristhian Parra <cdparra@gmail.com>
2013-11-27 09:16:11 +0100

    Fixed generated SOAP client as stub

--- lab9/SOAP-Clients/src/introsde/soap/client/SOAPHealthProfileReaderClientStub.java
+++ lab9/SOAP-Clients/src/introsde/soap/client/SOAPHealthProfileReaderClientStub.java
@@ -1,19 +1,31 @@
 package introsde.soap.client;
 
+import java.net.MalformedURLException;
+import java.rmi.RemoteException;
+
+import org.apache.axis.AxisFault;
+
 import samples.quickstart.HealthProfileReader;
 import samples.quickstart.HealthProfileReaderLocator;
 import samples.quickstart.HealthProfileReaderSoap11BindingStub;
+import service.health.pojos.xsd.HealthProfile;
 
 public class SOAPHealthProfileReaderClientStub {
 
-	public static void main(String[] args) {
-		try {
-			HealthProfileReaderLocator hprLoc = new HealthProfileReaderLocator();			
-			HealthProfileReaderSoap11BindingStub stub = new HealthProfileReaderSoap11BindingStub(hprLoc);
-			String value = stub.getParameterValue("Pinco", "Pallino", "bmi");
-			System.out.println(value);
-		} catch (Exception e) {
-			System.out.println(e.toString());
-		}
+	public static void main(String[] args) throws RemoteException,
+			MalformedURLException {
+		HealthProfileReaderLocator hprLoc = new HealthProfileReaderLocator();
+		java.net.URL endpoint = new java.net.URL(
+				hprLoc.getHealthProfileReaderHttpSoap11EndpointAddress());
+		HealthProfileReaderSoap11BindingStub stub = new HealthProfileReaderSoap11BindingStub(
+				endpoint, hprLoc);
+		String value = stub.getParameterValue("Pinco", "Pallino", "bmi");
+		System.out.println("Service 'getParameterValue' called asking for bmi. Result ="+value);
+
+		HealthProfile hp = stub.getHealthProfile("Pinco", "Pallino");
+
+		System.out.println("Health Profile obtained\n--> Height="
+				+ hp.getHeight() + "\n--> Weight=" + hp.getWeight()
+				+ "\n--> BMI=" + hp.getBMI());
 	}
 }
